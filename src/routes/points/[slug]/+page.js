3e4939d3493099bee6a875252607fd1585528eb4
Pachwenko <Pachwenko@users.noreply.github.com>
2023-12-24 00:50:31 +0000

    deploy: 3cf8af36ef0b9adddb4f88b9ad26917a34b44add

--- src/routes/points/[slug]/+page.js
+++
@@ -1,7 +0,0 @@
-export const load = ({ params }) => {
-    return {
-        // This will populate in the respective page's data
-        // This runs on the server if server-side rendering, and on client during navigation
-        slug: params.slug
-    }
-}
